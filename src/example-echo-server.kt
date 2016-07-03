12d27a16e2bcf5c489b69261e322d3c6867c8f7b
Carlos Ballesteros Velasco <soywiz@gmail.com>
2016-07-04 01:13:23 +0200

    Improved echo server displaying client id

--- src/example-echo-server.kt
+++ src/example-echo-server.kt
@@ -3,25 +3,28 @@ import es.kotlin.async.coroutine.async
 import es.kotlin.net.async.AsyncServer
 import es.kotlin.net.async.readLineAsync
 
+// Multi-client echo server using AsyncStream for listening clients
 fun main(args: Array<String>) = EventLoop.mainAsync {
 	val server = AsyncServer()
 	val port = 9090
 
 	println("Preparing...")
 	val charset = Charsets.UTF_8
+	var lastClientId = 0
 	val serverPromise = server.listen(port).eachAsync { client ->
-		println("Client connected!")
+		val clientId = lastClientId++
+		println("Client#$clientId connected!")
 		async<Unit> {
 			try {
 				while (true) {
 					val line = client.readLineAsync(charset).await()
-					println("Client said: $line")
+					println("Client#$clientId said: $line")
 					client.writeAsync(line.toByteArray(charset)).await()
 				}
 			} catch (t: Throwable) {
 				t.printStackTrace()
 			} finally {
-				println("Client disconnected!")
+				println("Client#$clientId disconnected!")
 			}
 		}
 	}
