6408095366fa158493091f3c73c097d8984d418f
Carlos Ballesteros Velasco <soywiz@gmail.com>
2016-07-20 14:55:10 +0200

    Bug?

--- test/es/kotlin/collection/coroutine/generateTest.kt
+++ test/es/kotlin/collection/coroutine/generateTest.kt
@@ -1,5 +1,9 @@
 package es.kotlin.collection.coroutine
 
+import es.kotlin.async.EventLoop
+import es.kotlin.async.coroutine.async
+import es.kotlin.async.utils.waitAsync
+import es.kotlin.time.seconds
 import org.junit.Assert
 import org.junit.Assert.*
 import org.junit.Test
@@ -15,5 +19,32 @@ class GenerateTest {
 	fun testGenerateNulls() {
 		fun gen() = generate<Int?> { for (n in 3 .. 7) { yield(n); yield(null) } }
 		assertEquals("3,null,4,null,5,null,6,null,7,null", gen().joinToString(","))
+	}
+
+	@Test
+	fun testAsyncAwait1() {
+		var out = ""
+		EventLoop.main {
+			async<Unit> {
+				out += "a"
+				waitAsync(0.1.seconds)
+				out += "b"
+			}
+		}
+		Assert.assertEquals("ab", out)
+	}
+
+	@Test
+	fun testAsyncAwait2() {
+		var out = ""
+		EventLoop.main {
+			async<Unit> {
+				out += "a"
+				waitAsync(0.1.seconds)
+				out += "b"
+				Unit
+			}
+		}
+		Assert.assertEquals("ab", out)
 	}
 }
