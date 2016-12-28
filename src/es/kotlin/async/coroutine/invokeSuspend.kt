d7efcb3243d5f5c6fbf718fb26bc7e4e7a8b8024
Roman Elizarov <elizarov@gmail.com>
2016-12-29 02:43:08 +0300

    Data races fixed using lock

--- src/es/kotlin/async/coroutine/invokeSuspend.kt
+++ src/es/kotlin/async/coroutine/invokeSuspend.kt
@@ -1,6 +1,7 @@
 package es.kotlin.async.coroutine
 
 import java.lang.reflect.Method
+import java.util.concurrent.locks.ReentrantLock
 import kotlin.coroutines.Continuation
 import kotlin.coroutines.CoroutineIntrinsics
 import kotlin.coroutines.suspendCoroutine
@@ -8,35 +9,52 @@ import kotlin.coroutines.suspendCoroutine
 
 // Useful for invoking methods with suspend
 class ContinuationWait<T> {
+	val lock = ReentrantLock()
 	var completed = false
 	var c_value: T? = null
 	var c_exception: Throwable? = null
 	var attachedContinuation: Continuation<T>? = null
 
+	inline fun <R> locked(block: () -> R): R {
+		lock.lock()
+		return try { block() } finally { lock.unlock() }
+	}
+
 	val continuation = object : Continuation<T> {
 		override fun resume(value: T) {
-			completed = true
-			c_value = value
-			attachedContinuation?.resume(value)
+			locked {
+				completed = true
+				c_value = value
+				attachedContinuation
+			}?.resume(value)
 		}
 
 		override fun resumeWithException(exception: Throwable) {
-			completed = true
-			c_exception = exception
-			attachedContinuation?.resumeWithException(exception)
+			locked {
+				completed = true
+				c_exception = exception
+				attachedContinuation
+			}?.resumeWithException(exception)
 		}
 
 	}
 
 	suspend fun await(): T = suspendCoroutine { c ->
-		if (completed) {
-			if (c_exception != null) {
-				c.resumeWithException(c_exception as Throwable)
+		var was_completed = false
+		var was_c_value: T? = null
+		var was_c_exception: Throwable? = null
+		locked {
+			was_completed = completed
+			was_c_value = c_value
+			was_c_exception = c_exception
+			if (!was_completed) attachedContinuation = c
+		}
+		if (was_completed) {
+			if (was_c_exception != null) {
+				c.resumeWithException(was_c_exception as Throwable)
 			} else {
-				c.resume(c_value as T)
+				c.resume(was_c_value as T)
 			}
-		} else {
-			attachedContinuation = c
 		}
 	}
 }
